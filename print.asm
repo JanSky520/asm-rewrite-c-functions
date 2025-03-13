ae8d58529954509125b044492e03eea7dd6ae2ae
jansky520 <1102812928@qq.com>
2025-03-14 02:30:38 +0800

    %d and %c and %s

--- print.asm
+++ print.asm
@@ -1,19 +1,25 @@
 section .bss
-    buffer resb 20
+    buffer resb 20    ;Reserve 20 bytes of space to store arbitrary data in 64-bit mode.
 
 section .text
     global print
 
     print:
         push rbp
-        mov rbp, rsp
+        mov rbp, rsp    ;Function prologue, the beginning of a function.
+
         push r12
         push r13
         push r14
-        push rbx
+        push r15
+        push rbx    ;Push the registers used in this function onto the stack to prevent data loss.
 
-        mov r12, rdi
-        mov r13, rsi
+        push rcx
+        push rdx
+
+        mov r12, rdi    ;
+        mov r13, rsi    ;
+        mov r15, 1    ;
 
         print_start:
             cmp byte [r12], 0
@@ -36,29 +42,30 @@ section .text
             mov r14, r12
             inc r14
             cmp byte [r14], 'd'
-            je printf_int
+            je print_int
             cmp byte [r14], 'c'
-            je printf_char
+            je print_str_char
             cmp byte [r14], 's'
-            je printf_str
+            je print_str_char
             jmp printf
 
-        printf_int:
+        print_int:
+            inc r15
             test r13, r13
-            jns printf_int_postive
+            jns print_int_postive
             neg r13
             mov rax, 1
             mov rdi, 1
             mov rdx, 1
             mov byte [rsi], '-'
             syscall
-            printf_int_postive:
+            print_int_postive:
                 mov rdi, buffer+19
                 mov byte [rdi], 0
                 mov rax, r13
                 mov rbx, 10
                 mov rcx, 0
-            printf_int_switch:
+            print_int_switch:
                 dec rdi
                 xor rdx, rdx
                 div rbx
@@ -66,40 +73,54 @@ section .text
                 add dl, '0'
                 mov byte [rdi], dl
                 test rax, rax
-                jnz printf_int_switch
+                jnz print_int_switch
             mov rdx, rcx
             mov rsi, rdi
             mov rax, 1
             mov rdi, 1
             syscall
             add r12, 2
-            jmp printf
+            cmp r15, 2
+            je printf_second
+            cmp r15, 3
+            je printf_third
+            jmp print_start
 
-        printf_char:
-            xor rax, rax
-            xor rdx, rdx
-            mov rax, r13
-            mov dl, al
-            mov rsi, rax
-            mov rax, 1
-            mov rdi, 1
-            mov rdx, 1
-            syscall
-            add r12, 2
-            jmp printf
-
-        printf_str:
-            push rdi
+        print_str_char:
+            inc r15
             mov rdi, r13
             call print
-            pop rdi
             add r12, 2
-            jmp printf
+            cmp r15, 2
+            je printf_second
+            cmp r15, 3
+            je printf_third
+            jmp print_start
+
+        printf_second:
+            pop r13
+            jmp print_start
+        printf_third:
+            pop r13
+            jmp print_start
 
         exit:
-            pop rbx
-            pop r14
-            pop r13
-            pop r12
+            cmp r15, 1
+            je exit_first
+            cmp r15, 2
+            je exit_second
+            cmp r15, 3
+            je exit_third
+
+            exit_first:
+                pop rdx
+            exit_second:
+                pop rcx
+            exit_third:
+                pop rbx
+                pop r15
+                pop r14
+                pop r13
+                pop r12
             leave
             ret

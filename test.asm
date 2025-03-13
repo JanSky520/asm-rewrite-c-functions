33f6d2f053a4a6224ba52bbc26a817c60f075179
jansky520 <1102812928@qq.com>
2025-03-14 02:30:17 +0800

    test

--- test.asm
+++ test.asm
@@ -1,7 +1,5 @@
 section .data
-    msg1 db "my name is %s", 10, 0
-    msg2 db "my age is %d", 10, 0
-    msg3 db "%c ascii is 97", 10, 0
+    msg db "my name is %s. my age is %d. '%c' ascii is 97.", 10, 0
 
     test_str db "JanSky", 0
     test_int dq 27
@@ -17,16 +15,10 @@ section .text
         push rbp
         mov rbp, rsp
 
-        mov rdi, msg1
+        mov rdi, msg
         mov rsi, test_str
-        call print
-
-        mov rdi, msg2
-        mov rsi, [test_int]
-        call print
-
-        mov rdi, msg3
-        mov rsi, test_char
+        mov rdx, [test_int]
+        mov rcx, test_char
         call print
 
         leave
